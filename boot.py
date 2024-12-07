bffbb3ddfe5dc3c8734dd934f222e21138751021
probonopd <probonopd@users.noreply.github.com>
2024-12-07 20:40:12 +0100

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,6 +1,13 @@
-import usb_hid
+import supervisor
 import usb_midi
+import usb_hid
+
 usb_hid.disable()
-usb_midi.set_names(manufacturer="CircuitPython", product="CircuitPython MIDI")
+
+# Vendor: usb 0x1c75 "Arturia"
+# Device: usb 0x028a "Arturia KeyLab Essential 61"
+supervisor.set_usb_identification(manufacturer="Arturia", product="Arturia KeyLab Essential 61", vid=0x1c75, pid=0x028a)
+usb_midi.set_names(manufacturer="Arturia", product="Arturia KeyLab Essential 61")
+
 usb_midi.enable()
 print("enabled USB MIDI, disabled USB HID")
